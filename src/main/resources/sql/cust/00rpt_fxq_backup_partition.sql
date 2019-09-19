d3e8274860906105a2f59c7da9988085c692cfac
jerry <jerry@j.lab>
2019-09-19 15:44:41 +0800

    append

--- src/main/resources/sql/cust/00rpt_fxq_backup_partition.sql
+++ src/main/resources/sql/cust/00rpt_fxq_backup_partition.sql
@@ -8,3 +8,4 @@ alter table rpt_fxq_tb_company_ms add partition (partition pt{lastday}000000 val
 alter table rpt_fxq_tb_company_ms add partition (partition future values less than (maxvalue));
 -- maybe start backup(history) or maybe finish backup(current), backup(history) data couldn't use current script
 alter table rpt_fxq_tb_company_ms reorganize partition future into (partition pt{lastday}000000 values less than('{lastday}999999'), partition future values less than(maxvalue)); 
+select  group_concat(table_name  separator ' ') from information_schema.partitions where table_name like 'ods_amltp%'  or table_name like 'ods_cthx%'
